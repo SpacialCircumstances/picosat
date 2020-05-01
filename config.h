6e9b0763619ab4f6d8ddf47e4ef417400f45a914
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-05-01 16:03:40 +0200

    Mark version as modified

--- config.h
+++ config.h
@@ -1,11 +1,7 @@
-//
-// Created by Felix on 28.04.2020.
-//
-
 #ifndef PICOSAT_CONFIG_H
 #define PICOSAT_CONFIG_H
 
-#define PICOSAT_VERSION "965"
+#define PICOSAT_VERSION "965-modified"
 #define PICOSAT_CC "gcc"
 #define PICOSAT_CFLAGS ""
 
